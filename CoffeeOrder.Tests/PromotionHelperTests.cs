211892276abea9ad8cadce467a311369fd6216df
Leanne Boulton <leanne.elizabeth@gmail.com>
2025-09-23 18:54:27 -0400

    added new test/commit

---
+++ CoffeeOrder.Tests/PromotionHelperTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoffeeOrder.Models;
+using CoffeeOrder.Pricing;
+using CoffeeOrder.Validation;
+using System.Linq;
+using System;
+using CoffeeOrder.Promotions;
+
+namespace CoffeeOrder.Tests;
+[TestClass]
+public class PromotionHelperTests
+{}
