b8e327573ae93770771d482bd4f3cb352d7892f4
Leanne Boulton <leanne.elizabeth@gmail.com>
2025-09-23 14:46:15 -0400

    feat(classify): hazelnut syrup triggers containsnuts flag

--- CoffeeOrder/obj/Debug/net8.0/CoffeeOrder.AssemblyInfo.cs
+++ CoffeeOrder/obj/Debug/net8.0/CoffeeOrder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeOrder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2046d301304cc5055f03315ab51bf5e728af217")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d405ed4f95880775f1586167a107ad29c22d8ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeOrder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeOrder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
